d1d1134a69f323fa3219072028b3e64697230a5a
Leonardo Faleiros <cassiojunior1220@gmail.com>
2021-07-22 21:55:12 -0300

    novo

---
+++ divisao.php
@@ -0,0 +1,26 @@
+<?php
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+
+if ($num2 != 0) {
+  $div = $num1 / $num2;
+} else {
+   $div = 0;
+ }
+
+$div = $num1 / $num2;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Divisão</title>
+</head>
+<body>
+  <h1></h1>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Divisão</title>
+</head>
+<body>
+  <form action="divisao.php" method="post"></form>
+  <select name="num1">
+   <option value="0">0</option>
+   <option value="1">1</option>
+   <option value="2">2</option>
+   <option value="3">3</option>
+   <option value="4">4</option>
+  </select>
+  <select name="num2">
+   <option value="0">0</option>
+   <option value="1">1</option>
+   <option value="2">2</option>
+   <option value="3">3</option>
+   <option value="4">4</option>
+  </select>
+   <button type="submit">Enviar</button>
+</body>
+</html>
